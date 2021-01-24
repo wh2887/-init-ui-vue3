6e2e570c5001d35348450fd226c6234172fb8e01
mccall <1059511459@qq.com>
2021-01-24 18:39:43 +0800

    Switch 组件的 CSS 样式及动画过渡

---
+++ src/lib/Switch.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button><span></span></button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  width: $h*2;
+  height: $h;
+  background: blue;
+  border: none;
+  border-radius: $h/2;
+  position: relative;
+}
+
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  width: $h2;
+  height: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+  transition: left 250ms;
+}
+
+button:hover > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
+}
+
+</style>
