6ee713f7b4ee5ca2b26a97c78e418b4fadc76b08
McCallWang <13567272790@163.com>
2021-01-27 17:13:16 +0800

    Button 组件添加 level 属性,颜色搭配修改

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -6,12 +6,29 @@
     <Button> normal</Button>
     <Button size="big"> big</Button>
   </div>
-  <hr/>
-  <h3>示例1： theme 属性</h3>
+  <h3>示例2： theme 属性</h3>
   <div>
     <Button theme="button"> button</Button>
     <Button theme="link"> link</Button>
     <Button theme="text"> text</Button>
+  </div>
+  <h3>示例3： level 属性</h3>
+  <div>
+    <div>
+      <Button> normal</Button>
+      <Button level="main"> main</Button>
+      <Button level="danger"> danger</Button>
+    </div>
+    <div>
+      <Button theme="link"> normal</Button>
+      <Button theme="link" level="main"> link main</Button>
+      <Button theme="link" level="danger"> link danger</Button>
+    </div>
+    <div>
+      <Button theme="text"> normal</Button>
+      <Button theme="text" level="main"> text main</Button>
+      <Button theme="text" level="danger"> text danger</Button>
+    </div>
   </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -20,13 +20,18 @@ export default {
       type: String,
       default: 'button'
     },
+    level: {
+      type: String,
+      default: 'normal'
+    }
   },
   setup(props) {
-    const {size, theme} = props;
+    const {size, theme, level} = props;
     const classes = computed(() => {
       return {
         [`init-size-${size}`]: size,
         [`init-theme-${theme}`]: theme,
+        [`init-level-${level}`]: level,
       };
     });
 
@@ -58,89 +63,111 @@ $radius: 4px;
 $h: 32px;
 
 .init-button {
-  background: $light-green;
-  height: $h;
-  padding: 0 12px;
-  cursor: pointer;
-  border-radius: $radius;
-  border: none;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  white-space: nowrap;
-  color: $light-grey-6;
-  position: relative;
-  overflow: hidden;
+  background: $dark-grey-4; height: $h;
+  padding: 0 12px; cursor: pointer; border-radius: $radius; border: none;
+  display: inline-flex; justify-content: center; align-items: center; white-space: nowrap;
+  color: $light-grey-6; position: relative; overflow: hidden;
 
   & + & {
     margin-left: 8px;
   }
 
-  &:hover {
-    background: rgba($light-green, .9);
-  }
+  &:hover { background: rgba($dark-grey-4, .9); }
 
-  &:focus {
-    outline: none;
-  }
+  &:focus { outline: none; }
 
-  &::-moz-focus-inner {
-    border: 0;
-  }
+  &::-moz-focus-inner { border: 0; }
 
   &.init-size-big {
-    font-size: 24px;
-    height: 48px;
-    padding: 0 16px;
+    font-size: 24px; height: 48px; padding: 0 16px;
   }
 
   &.init-size-small {
-    font-size: 12px;
-    height: 20px;
-    padding: 0 4px;
+    font-size: 12px; height: 20px; padding: 0 4px;
+  }
+
+  &.init-theme-button {
+    &.init-level-main {
+      background: $light-green;
+
+      &:hover, &:focus {
+        background: rgba($light-green, .9);
+      }
+    }
+
+    &.init-level-danger {
+      background: $light-red;
+
+      &:hover, &:focus {
+        background: rgba($light-red, .9);
+      }
+    }
+
   }
 
   &.init-theme-link {
-    background: transparent;
-    border-color: transparent;
-    box-shadow: none;
-    color: $light-green;
-    &:hover,
-    &:focus {
-      color: lighten($light-green, 10%);
+    background: transparent; border-color: transparent; box-shadow: none; color: $dark-grey-4;
+
+    &:hover, &:focus {
+      color: lighten($dark-grey-4, 10%);
     }
-    &:active {
-      color: darken($dark-green, 10%);
+
+    &:active { color: darken($dark-grey-4, 10%); }
+    &.init-level-main{
+      color: $light-green;
+      &:hover, &:focus {
+        color: lighten($light-green, 10%);
+      }
+
+      &:active { color: darken($light-green, 10%); }
+    }
+
+    &.init-level-danger {
+      color: $light-red;
+
+      &:hover, &:focus {
+        color: lighten($light-red, 10%);
+      }
+
+      &:active { color: darken($dark-red, 10%); }
     }
   }
 
   &.init-theme-text {
-    background: transparent;
-    border-color: transparent;
-    color: $light-green;
-    &:hover,
-    &:focus {
-      color: lighten($light-green, 10%);
-      background: rgba($light-green, .1);
+    background: transparent; border-color: transparent; color: $dark-grey-4;
+
+    &:hover, &:focus {
+      color: lighten($dark-grey-4, 10%); background: rgba($dark-grey-4, .1);
     }
-    &:active {
-      background: rgba($light-green, .2);
+
+    &:active { background: rgba($dark-grey-4, .2); }
+
+    &.init-level-main {
+      color: $light-green;
+      &:hover, &:focus {
+        color: lighten($light-green, 10%); background: rgba($light-green, .1);
+      }
+      &:active { background: rgba($light-green, .2); }
+
+    }
+
+    &.init-level-danger {
+      color: $light-red;
+      &:hover, &:focus {
+        color: lighten($light-red, 10%); background: rgba($light-red, .1);
+      }
+
+      &:active { background: rgba($light-red, .2); }
     }
   }
 
   > .init-button-ripple {
-    position: absolute;
-    border-radius: 50%;
-    transform: scale(0);
-    animation: ripple 600ms linear;
-    background-color: rgba(255, 255, 255, 0.5);
+    position: absolute; border-radius: 50%; transform: scale(0);
+    animation: ripple 600ms linear; background-color: rgba(255, 255, 255, 0.5);
   }
 }
 
 @keyframes ripple {
-  to {
-    transform: scale(4);
-    opacity: 0;
-  }
+  to { transform: scale(4); opacity: 0; }
 }
 </style>
