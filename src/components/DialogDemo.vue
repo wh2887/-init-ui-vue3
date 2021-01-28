a53c3cb2f0d616b4292d56d301ee7056de583209
McCallWang <13567272790@163.com>
2021-01-28 11:27:05 +0800

    Dialog 组件使用 Teleport

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,18 @@
 <template>
   <h2>Dialog 示例</h2>
-  <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
-    <template v-slot:title>
-      <strong>传入标题</strong>
-    </template>
-    <template v-slot:content>
-      传入内容 <br>
-      传入内容
-    </template>
-  </Dialog>
+  <div style="position: relative; z-index: 1;">
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi2</div>
+      </template>
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+    </Dialog>
+  </div>
+  <div style="position: relative; z-index: 2; width: 300px; height: 300px; background: red;"></div>
 </template>
 
 <script lang="ts">
