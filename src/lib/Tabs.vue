285d480303d471385792b1643248cb65753259ae
McCallWang <13567272790@163.com>
2021-01-29 17:30:34 +0800

    Tabs 组件重构

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="init-tabs">
-    <div class="init-tabs-nav">
-      <Button class="init-tabs-nav-item" theme="text" level="main"
-              v-for="(t,index) in titles" :key="index"
-              :class="{selected:t === selected}"
-              @click="select(t)"
-      >
+    <div class="init-tabs-nav" ref="container">
+      <div class="init-tabs-nav-item" v-for="(t,index) in titles" :key="index" @click="select(t)"
+           :ref="el =>{if(t===selected)selectedItem=el}" :class="{selected:t === selected}">
         {{ t }}
-      </Button>
+      </div>
+      <div class="init-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="init-tabs-content">
-      <component class="init-tabs-content-item"
-                 v-for="(c,index) in defaults" :is="c" :key="index"
-                 :class="{selected:c.props.title === selected}"
-      />
+      <component :is="current" :key="current.props.title"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {computed} from 'vue';
+import {computed, ref, watchEffect, onMounted, onUpdated} from 'vue';
 import Tab from './Tab.vue';
 import Button from './Button.vue';
 
@@ -31,53 +26,74 @@ export default {
     }
   },
   setup(props, content) {
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    let left;
+    let number = ref(0);
+    const moveBottom = () => {
+      const {width} = selectedItem.value.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const {left: left1} = container.value.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
+      left = left2 - left1;
+      indicator.value.style.left = left + 'px';
+    };
+    onMounted(() => { moveBottom(); });
+    onUpdated(() => { moveBottom(); });
     const defaults = content.slots.default();
-    // console.log(defaults[0].type === Tab);
-    defaults.forEach(tag => {
-      if (tag.type !== Tab) {
-        throw new Error('Tabs 子标签必须是 Tab！');
-      }
-    });
-    const titles = defaults.map(tag => tag.props.title);
-    const current = computed(() => {
-      console.log('重新 return');
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected;
-      })[0];
-    });
+    defaults.forEach(tag => { if (tag.type !== Tab) { throw new Error('Tabs 子标签必须是 Tab！');}});
+    const current = computed(() => { return defaults.find(tag => tag.props.title === props.selected); });
+    const titles = defaults.map(tag => {return tag.props.title;});
     const select = (title: string) => {
+      number.value += 1;
       content.emit('update:selected', title);
     };
-    return {defaults, titles, current, select};
+    return {current, defaults, titles, select, selectedItem, indicator, container};
   }
 };
 </script>
 
 <style lang="scss">
+$bg: white;
+$selected-bg: #ECECEC;
+$color: #313131;
+
 .init-tabs {
   &-nav {
     display: flex;
+    color: #333;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    width: 100%;
 
     &-item {
+      padding: 8px 13px;
+      cursor: pointer;
+
       &:first-child {
+        margin-left: 0;
       }
 
       &.selected {
-        color: inherit;
+        color: #313131;
+        background: $selected-bg;
       }
+    }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $color;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
 
   &-content {
     padding: 8px 0;
-
-    &-item {
-      display: none;
-
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
