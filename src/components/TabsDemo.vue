09d9d4878b96d7b0e74337d8b5a6fdd5b87fd571
McCallWang <13567272790@163.com>
2021-01-29 11:01:21 +0800

    Tabs 组件内容部分使用 CSS 控制显示隐藏

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,17 +1,22 @@
 <template>
   <h2>Tabs 示例</h2>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
 </template>
 
 <script lang="ts">
+import {ref} from 'vue';
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
 
 export default {
-  components: {Tabs, Tab}
+  components: {Tabs, Tab},
+  setup() {
+    const x = ref('导航1');
+    return {x};
+  },
 };
 </script>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,35 @@
 <template>
-  <div>
-    Tabs 组件
-    <Button theme="text" level="main" v-for="(t,index) in titles" :key="index">{{ t }}</Button>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="init-tabs">
+    <div class="init-tabs-nav">
+      <Button class="init-tabs-nav-item" theme="text" level="main"
+              v-for="(t,index) in titles" :key="index"
+              :class="{selected:t === selected}"
+              @click="select(t)"
+      >
+        {{ t }}
+      </Button>
+    </div>
+    <div class="init-tabs-content">
+      <component class="init-tabs-content-item"
+                 v-for="(c,index) in defaults" :is="c" :key="index"
+                 :class="{selected:c.props.title === selected}"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
 import Tab from './Tab.vue';
 import Button from './Button.vue';
 
 export default {
   components: {Button},
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, content) {
     const defaults = content.slots.default();
     // console.log(defaults[0].type === Tab);
@@ -21,11 +39,45 @@ export default {
       }
     });
     const titles = defaults.map(tag => tag.props.title);
-    return {defaults, titles};
+    const current = computed(() => {
+      console.log('重新 return');
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
+    const select = (title: string) => {
+      content.emit('update:selected', title);
+    };
+    return {defaults, titles, current, select};
   }
 };
 </script>
 
 <style lang="scss">
+.init-tabs {
+  &-nav {
+    display: flex;
 
+    &-item {
+      &:first-child {
+      }
+
+      &.selected {
+        color: inherit;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
 </style>
