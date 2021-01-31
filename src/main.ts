a81c0a2bafc43d2fec0e28ad7c738c5e90051dc7
mccall <1059511459@qq.com>
2021-01-31 13:36:48 +0800

    引入 github-markdown-css

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './assets/styles/reset.scss'
+import './assets/styles/reset.scss';
 import './lib/styles/reset.scss';
 import {createApp} from 'vue';
 import App from './App.vue';
 import {router} from './router/router';
+import 'github-markdown-css';
 
 
 const app = createApp(App);
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-        Install 安装页
-    </div>
+  <article class="markdown-body">
+    <h1>安装</h1>
+    <p>打开终端，运行下列命令：</p>
+    <pre><code>npm install init-ui-vue3</code></pre>
+    <p>或</p>
+    <pre><code>yarn add init-ui-vue3</code></pre>
+    <p>下一节：<a href="#/doc/get-started">开始使用</a></p>
+  </article>
 </template>
 
 <script lang="ts">
-export default {}
+export default {};
 </script>
 
 <style lang="scss" scoped>
