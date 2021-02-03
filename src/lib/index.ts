7e7231f8b5a817a7df8744dff7e1956a5ab64b5f
McCallWang <13567272790@163.com>
2021-02-03 10:32:57 +0800

    创建 lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,10 @@
+export {default as Switch} from './Switch.vue';
+export {default as Button} from './Button.vue';
+export {default as Tabs} from './Tabs.vue';
+export {default as Tab} from './Tab.vue';
+export {default as Dialog} from './Dialog.vue';
+export {openDialog as openDialog} from './openDialog';
+
+// 等价于 ：
+// import Switch from './Switch.vue';
+// export default {Switch};
